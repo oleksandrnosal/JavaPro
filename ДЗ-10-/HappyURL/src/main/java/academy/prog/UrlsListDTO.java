a3d2aae2a2aed5fbfa285950d43225627948d5dd
Олександр Носаль <113480733+oleksandrnosal@users.noreply.github.com>
2023-10-06 20:25:35 +0300

    Add files via upload

---
+++ ДЗ-10-/HappyURL/src/main/java/academy/prog/UrlsListDTO.java
@@ -0,0 +1,13 @@
+package academy.prog;
+
+import java.util.List;
+
+public class UrlsListDTO {
+    private List<String> Urls;
+    public List<String> getUrls() {
+        return Urls;
+    }
+public void setUrls(List<String> Urls){
+    this.Urls = Urls;
+    }
+}
